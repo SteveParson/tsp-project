68577fbc621c7915e781f52e8905392da2d7128d
Eeshan Garg <jerryguitarist@gmail.com>
2018-11-21 18:50:43 -0330

    Clean up src/select.py.

--- src/select.py
+++ src/select.py
@@ -4,95 +4,100 @@ import random
 
 
 def parents(args):
-    """ Selects parents to form a mating pool
-
-    :param args: The global parameter dictionary
-    :return: Adds the mating_pool indices to the dictionary
     """
-    # random selection for testing purposes
-    pop_size = args['pop_size']
+    Selects parents to form a mating pool.
 
-    # use mp_size if availab,e otherwise set it to half of the population size
-    # this default selection and assignment should happen somewhere else
-    mp_size = args.get('mp_size', int(pop_size / 2))
-    args['mp_size'] = mp_size
-
-    # even number of mates
-    mp_size = mp_size + 1 if (mp_size % 2 == 1) else mp_size
+    :param args: The global EA parameter dictionary.
+    :return: Adds the mating_pool indices to the parameter dictionary.
+    """
 
     if args['parent_selection'] == 'random':
-        args['mating_pool'] = random_selection(range(pop_size), mp_size, False)
+        args['mating_pool'] = random_selection(range(args['pop_size']),
+                                               args['mp_size'], False)
 
     if args['parent_selection'] == 'tournament_selection':
-        selected_to_mate = []
-        fitness = list(enumerate(args['fitness']))
+        parent_tournament_selection(args)
 
-        current_member = 0
-        while current_member < mp_size:
-            random_individuals = random.sample(fitness, args['tournament_size'])
-            best = max(random_individuals, key=lambda arr: arr[1])
-            selected_to_mate.append(best[0])
-            current_member = current_member + 1
+def parent_tournament_selection(args):
+    """
+    Selects parents using tournament selection.
 
-        args['mating_pool'] = selected_to_mate
+    :param args: The global EA parameter dictionary.
+    """
+    selected_to_mate = []
+    fitness = list(enumerate(args['fitness']))
+
+    current_member = 0
+    while current_member < args['mp_size']:
+        random_individuals = random.sample(fitness, args['tournament_size'])
+        best = max(random_individuals, key=lambda arr: arr[1])
+        selected_to_mate.append(best[0])
+        current_member = current_member + 1
+
+    args['mating_pool'] = selected_to_mate
 
 def survivors(args):
-    """ Selects survivors in the population
+    """
+    Selects survivors in the population.
 
-    :param args: Our global dictionary
+    :param args: The global EA parameter dictionary.
     :return: Reassigns 'population' and 'fitness' in the dictionary
     """
 
     survivor_selection = args['survivor_selection']
-
     if survivor_selection == 'mu_plus_lambda':
-        # pool the population and offspring
-        full_population = args['population'] + args['offspring']
-        full_fitness = args['fitness'] + args['offspring_fitness']
-        mu = args['pop_size']
+        mu_plus_lambda_survivor_selection(args)
 
-        # rank their fitnesses, keep only mu
-        rank_vector = rankify(full_fitness)[:mu]
-
-        # keep only the top mu individuals
-        population, fitness = [], []
-
-        for index in rank_vector:
-            population.append(full_population[index])
-            fitness.append(full_fitness[index])
-
-        # reassign the dictionary
-        args['population'] = population
-        args['fitness'] = fitness
-        return
-
-    # TODO: use numpy to do this
     if survivor_selection == 'random':
-        full_population = args['population'] + args['offspring']
-        full_fitness = args['fitness'] + args['offspring_fitness']
-        full_pool = len(full_population)
-        mu = args['pop_size']
+        random_survivor_selection(args)
 
-        # get a random selection
-        choices = random_selection(range(full_pool), mu, False)
-        population, fitness = [], []
+def mu_plus_lambda_survivor_selection(args):
+    """
+    Select survivors using mu + lambda selection.
 
-        # make the choices
-        for index in choices:
-            population.append(full_population[index])
-            fitness.append(full_fitness[index])
+    :param args: The global EA parameter dictionary.
+    """
+    full_population = args['population'] + args['offspring']
+    full_fitness = args['fitness'] + args['offspring_fitness']
+    mu = args['pop_size']
 
-        # reassign the dictionary
-        args['population'] = population
-        args['fitness'] = fitness
-    return
+    rank_vector = rankify(full_fitness)[:mu]
+    population, fitness = [], []
 
+    for index in rank_vector:
+        population.append(full_population[index])
+        fitness.append(full_fitness[index])
+
+    args['population'] = population
+    args['fitness'] = fitness
+
+def random_survivor_selection(args):
+    """
+    Random survivor selection.
+
+    :param args: The global EA parameter dictionary.
+    """
+    full_population = args['population'] + args['offspring']
+    full_fitness = args['fitness'] + args['offspring_fitness']
+    full_pool = len(full_population)
+    mu = args['pop_size']
+
+    choices = random_selection(range(full_pool), mu, False)
+    population, fitness = [], []
+
+    for index in choices:
+        population.append(full_population[index])
+        fitness.append(full_fitness[index])
+
+    args['population'] = population
+    args['fitness'] = fitness
 
 def random_selection(individuals, number_to_choose, with_replace=False):
-    """ Returns a random sample from a set, with or without replacement
+    """
+    Returns a random sample from a set, with or without replacement
 
-    :param individuals: The choices
-    :param number_to_choose: How many to choose
+    :param individuals: The individuals to choose from.
+    :param number_to_choose: The size of the sample.
     :param with_replace: Pick with replacement? (Default: False)
     :return: The random sample
     """
